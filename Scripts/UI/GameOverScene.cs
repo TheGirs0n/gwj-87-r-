271a71cfcf45e7b17c2e271982f727de313bdc73
Timur <huntertring87@gmail.com>
2025-11-20 16:34:23 +0300

    core

--- Scripts/UI/GameOverScene.cs
+++ Scripts/UI/GameOverScene.cs
@@ -10,6 +10,20 @@ public partial class GameOverScene : Control
 	[Export] private Theme _textDayLabel;
 	[Export] private Theme _textNightLabel;
 	
+	[ExportGroup("DayNightCount")]
+	[Export] private RichTextLabel _dayNightCountText;
+	[Export] private RichTextLabel _dayNightCount;
+	
+	[ExportGroup("Modifiers")] 
+	[Export] private RichTextLabel _speedMultiplayer;
+	[Export] private RichTextLabel _speedMultiplayerValue;
+	[Export] private RichTextLabel _spawnTime;
+	[Export] private RichTextLabel _spawnTimeValue;
+	[Export] private RichTextLabel _positiveScore;
+	[Export] private RichTextLabel _positiveScoreValue;
+	[Export] private RichTextLabel _negativeScore;
+	[Export] private RichTextLabel _negativeScoreValue;
+	
 	[ExportGroup("Texture")]
 	[Export] private TextureRect _textureRect;
 	[Export] private CompressedTexture2D _textureDayRect;
@@ -59,19 +73,62 @@ public partial class GameOverScene : Control
 		switch (timeType)
 		{
 			case TimeType.DAY:
-				_richTextLabel.Theme = _textDayLabel;
-				_textureRect.Texture = _textureDayRect;
-				_buttonRestartGame.Theme = _buttonRestartDayTheme;
-				_buttonMainMenu.Theme = _buttonRestartDayTheme;
+				SetupTexture(_textDayLabel, _textureDayRect);
+
+				SetupButtons(_buttonRestartDayTheme);
+
+				SetupDayNight(_textDayLabel);
+				
+				SetupModifiers(_textDayLabel);
 				break;
 			case TimeType.NIGHT:
-				_richTextLabel.Theme = _textNightLabel;
-				_textureRect.Texture = _textureNightRect;
-				_buttonRestartGame.Theme = _buttonRestartNightTheme;
-				_buttonMainMenu.Theme = _buttonRestartNightTheme;
+				SetupTexture(_textNightLabel, _textureNightRect);
+
+				SetupButtons(_buttonRestartNightTheme);
+				
+				SetupDayNight(_textNightLabel);
+				
+				SetupModifiers(_textNightLabel);
 				break;
 		}
 		
 		_animatePlayer.Play("playOver");
+	}
+
+	private void SetupTexture(Theme theme, CompressedTexture2D texture)
+	{
+		_richTextLabel.Theme = theme;
+		_textureRect.Texture = texture;
+	}
+
+	private void SetupButtons(Theme theme)
+	{
+		_buttonRestartGame.Theme = theme;
+		_buttonMainMenu.Theme = theme;
+	}
+
+	private void SetupDayNight(Theme theme)
+	{
+		_dayNightCountText.Theme = theme;
+		_dayNightCount.Theme = theme;
+	}
+
+	private void SetupModifiers(Theme theme)
+	{
+		_speedMultiplayer.Theme = theme;
+		_speedMultiplayerValue.Theme = theme;
+		_speedMultiplayerValue.Text = GlobalContext.SpawnMiniCoresInstance.MiniCoreSpeedMultiplier.ToString();
+		
+		_spawnTime.Theme = theme;
+		_spawnTimeValue.Theme = theme;
+		_spawnTimeValue.Text = GlobalContext.SpawnMiniCoresInstance.SpawnTimer.WaitTime.ToString();
+		
+		_positiveScore.Theme = theme;
+		_positiveScoreValue.Theme = theme;
+		_positiveScoreValue.Text = GlobalContext.SpawnMiniCoresInstance.MiniCorePositiveScore.ToString();
+		
+		_negativeScore.Theme = theme;
+		_negativeScoreValue.Theme = theme;
+		_negativeScoreValue.Text = GlobalContext.SpawnMiniCoresInstance.MiniCoreNegativeScore.ToString();
 	}
 }
