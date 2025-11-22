618590d86f48152a9e92c2744f0438595267a148
Timur <huntertring87@gmail.com>
2025-11-23 01:48:27 +0300

    qwzXz

--- Scripts/MainGame/AudioManager.cs
+++ Scripts/MainGame/AudioManager.cs
@@ -5,16 +5,10 @@ public partial class AudioManager : Node
 	[ExportGroup("Audio")]
 	[Export] private AudioStreamPlayer _miniCoreEntrySound;
 	[Export] private AudioStreamPlayer _miniCoreDestroySound;
-	[Export] private AudioStreamPlayer _backgroundMusicSound;
 	
 	public override void _EnterTree()
 	{
 		GlobalContext.AudioManagerInstance = this;
-	}
-
-	public void PlayBackgroundMusic()
-	{
-		_backgroundMusicSound.Play();
 	}
 	
 	public void PlayCoreEntrySound()
